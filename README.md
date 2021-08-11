34b09bee48081fb0bbac397c811c61b50a1a6691
Max Programming <ui.sabuwala@gmail.com>
2021-08-11 16:59:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Meme API
+
+Inspired from [Random Memer](https://github.com/techytushar/random-memer)
+
+Node API to return random programming meme images scrapped from [Memedroid](https://www.memedroid.com/memes/tag/programming).
+
+## Setup
+
+Install the dependencies
+
+```bash
+yarn
+# or npm i
+yarn start
+```
+
+## Tech Stack
+
+Node, Puppeteer, Fastify
+
+## API Reference
+
+#### Get all memes
+
+```http
+  GET /memes
+```
+
+#### Get a random meme
+
+```http
+  GET /random
+```
+
+## Feedback
+
+If you have any feedback, please reach out to us at fake@fake.com
---
+++ index.js
@@ -0,0 +1,46 @@
+const puppeteer = require('puppeteer');
+const fastify = require('fastify')({ logger: true });
+
+async function getAllMemes() {
+  const URL = 'https://www.memedroid.com/memes/tag/programming';
+
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+
+  await page.goto(URL);
+
+  const allImages = await page.$$eval('div.item-aux-container img[src]', imgs =>
+    imgs.map(img => {
+      if (
+        img.getAttribute('src').startsWith('http') &&
+        img.getAttribute('src').endsWith('jpeg')
+      )
+        return img.getAttribute('src');
+    })
+  );
+
+  const imgs = allImages.filter(img => img !== null);
+
+  return imgs;
+}
+
+fastify.get('/memes', async (request, reply) => {
+  const memes = await getAllMemes();
+  reply.send({ memes });
+});
+
+fastify.get('/', async (request, reply) => {
+  const memes = await getAllMemes();
+  const randomNumber = Math.round(Math.random() * memes.length);
+  reply.send({ randomMeme: memes[randomNumber] });
+});
+
+const start = async () => {
+  try {
+    await fastify.listen(5555);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
